9b346e271b7369f6b5c30a216e896d7316cdc435
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2024-09-01 14:32:33 +0530

    Add Initial Files

---
+++ script.js
@@ -0,0 +1,23 @@
+let submit = document.getElementById("btn");
+let resContent = document.getElementById("result-content");
+let apikey = "ema_live_YUJWjiVzeW5byC1ieu1SGHYYFmw8j1TH7qVHBo75";
+let str = ``;
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    resContent.innerHTML = `<img src="loading2.svg" width="50px">`;
+    validateEmail();
+})
+
+const validateEmail = async () => {
+    let email = document.getElementById("email").value;
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${apikey}&email=${email}`;
+    console.log(url);
+    let reponse = await fetch(url);
+    console.log(reponse);
+    let data = await reponse.json();
+    console.log(data);
+    for (const key in data) {
+        str += `<div>${key}: ${data[key]}</div>`
+    }
+    resContent.innerHTML = str;
+}
